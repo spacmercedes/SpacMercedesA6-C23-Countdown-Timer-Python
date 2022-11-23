f02c507584da656c121dc2151fb683a2750c01ae
spacmercedes <79169837+spacmercedes@users.noreply.github.com>
2022-11-23 19:07:19 +0200

    Add files via upload
    Introducerea librarii pygame

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # Countdown-Timer-Python
 Final python project
-
-For running install:
-
-  ~ pip install win10toast
-  
-  ~ pip3 install playsound
-  
-  ~ pip install pygame 
-  
-  
--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-import time
-from playsound import playsound as play
+import pygame, sys, time, os
 from win10toast import ToastNotifier
+from playsound import playsound as play
 
+pygame.init()
+pygame.display.set_caption('Coundown Timer')
 
-if __name__ == '__main__':
-    h = int(input("Enter the hours: "))
-    m = int(input("Enter the minutes: "))
-    s = int(input("Enter the seconds: "))
+fontSize = 100
+font = pygame.font.SysFont('Ubuntu-M.ttf', fontSize)
 
+clock = pygame.time.Clock()
+W,H = 800, 400
+screen = pygame.display.set_mode([W,H])
+
+boolean = True
+
+def notification():
     toast = ToastNotifier()
-
-
-
-    for i in range(h, -1, -1):
-        for j in range(m, -1, -1):
-            for k in range(s, -1, -1):
-                time.sleep(1)
-                print(f"{i:02d}:{j:02d}:{k:02d}")
-            s = 59
-        m = 59
-    print("Time is up! ")
     toast.show_toast(
         "Countdown Timer",
         "Time is up!",
@@ -27,5 +23,40 @@ if __name__ == '__main__':
         icon_path="clock.ico",
         threaded=True
     )
-    play('alarm.mp3')
+    audio_file = os.path.dirname(__file__) + '\_alarm.mp3'
+    play(audio_file)
+    print("Time is up")
 
+def draw_text(i,j,k):
+    text = f"{i:02d} : {j:02d} : {k:02d}"
+    label = font.render(text, True, (255,255,255))
+    screen.blit(label, (W/2-180,H/2-35))
+    # pygame.time.delay(1000)
+    time.sleep(1)
+    pygame.display.update()
+
+
+def timer(h,m,s):
+    for i in range(h, -1, -1):
+        for j in range(m, -1, -1):
+            for k in range(s, -1, -1):
+                pygame.draw.rect(screen, (76, 82, 103), pygame.Rect(W / 2 - 200, H / 2 - 50, 400, 100))
+                draw_text(i, j, k)
+            s = 59
+        m = 59
+
+    notification()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((26,43,66))
+
+    timer(0,0,3)
+
+
+    pygame.display.flip()
+    clock.tick(100)
