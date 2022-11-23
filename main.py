8c06a212a02363736d452b2378f5256aa1bfe81f
spacmercedes <79169837+spacmercedes@users.noreply.github.com>
2022-11-23 14:49:43 +0200

    Functionarea de baza a temporizatorului fara GUI

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import time
 
 
 if __name__ == '__main__':
-    n = int(input("Enter the seconds: "))
+    h = int(input("Enter the hours: "))
+    m = int(input("Enter the minutes: "))
+    s = int(input("Enter the seconds: "))
 
-    for i in range(n, 0, -1):
-        time.sleep(1)
-        print(i)
+    for i in range(h, -1, -1):
+        for j in range(m, -1, -1):
+            for k in range(s, -1, -1):
+                time.sleep(1)
+                print(f"{i:02d}:{j:02d}:{k:02d}")
+            s = 59
+        m = 59
     print("Time is up! ")
 
